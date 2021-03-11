e1245e0efc093e80e35d4537025035bf14044efd
mbabic-arena <m_babic@teol.net>
2021-03-11 21:33:26 +0100

    Added folders with exercises for Day 4

---
+++ ex10/src/index.js
@@ -0,0 +1,16 @@
+function main() {
+    // Only change code below this line
+    var myFirstName = "Miroslav";
+    var myLastName = "Babic";
+    var myHomeTown = "Celinac";
+    // Only change code above this line
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main());
+
+module.exports = main;
